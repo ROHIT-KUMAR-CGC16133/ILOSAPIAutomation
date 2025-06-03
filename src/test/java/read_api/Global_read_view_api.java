d1cc81f54470b35d3fdef5a3f85a9c5acd341ad5
Rohit Kumar <rohit.kumar4@capriglobal.in>
2025-06-03 18:16:40 +0530

    more testcases added till disbursement

--- src/test/java/read_api/Global_read_view_api.java
+++ src/test/java/read_api/Global_read_view_api.java
@@ -2,6 +2,7 @@ package read_api;
 
 import generic.Generic;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.testng.annotations.Test;
 import utils.PropertiesReadWrite;
@@ -20,6 +21,7 @@ public class Global_read_view_api {
     private final String appId = PropertiesReadWrite.getValue("application_id");
     private final String userId = PropertiesReadWrite.getValue("global_readOnly_user");
     private final String userPass = PropertiesReadWrite.getValue("global_readOnly_password");
+    private final String objId = PropertiesReadWrite.getValue("obj_id");
 
     private Map<String, Object> headers;
 
@@ -189,7 +191,7 @@ public class Global_read_view_api {
     public void verify_deviation_data_legal_api() throws IOException {
         validateApiResponse(
                 baseUrl + "/ilos/v1/misc/deviation-data",
-                Map.of("obj_id", PropertiesReadWrite.getValue("obj_id"), "module", "legal"),
+                Map.of("obj_id", objId, "module", "legal"),
                 "schemas/deviation_data_legal_schema.json",
                 "response_validation/deviation_data_legal_res.json"
         );
@@ -212,5 +214,126 @@ public class Global_read_view_api {
                 "response_validation/deviation_condition_legal_res.json"
         );
     }
+    @Test(priority = 19)
+    public void verify_legal_property_list_api() throws IOException {
+        validateApiResponse(
+                baseUrl + "/ilos/v1/legal/property/list/" + appId,
+                Map.of("role", "GLOBAL_READ_ONLY"),
+                "schemas/legal_property_list_schema.json",
+                "response_validation/legal_property_list_res.json"
+        );
+    }
+    @Test(priority = 20)
+    public void verify_disb_sanction_cond_legal_api() throws IOException {
+        validateApiResponse(
+                baseUrl + "/ilos/v1/disbursement/sanction-condition" ,
+                Map.of("obj_id", objId, "module", "legal"),
+                "schemas/disb_sanction_cond_legal_schema.json",
+                "response_validation/disb_sanction_cond_legal_res.json"
+        );
+    }
+    @Test(priority = 21)
+    public void verify_pdmetalist_api() throws IOException {
+        headers = getHeaders(userId, userPass);
+        Response response = RestUtils.performGet(baseUrl + "/pd/pdmeta/list/"+appId, headers,  Map.of("user", ""));
+      //  response.prettyPrint();
+        Generic.validateResponse(response);
+        response.then().assertThat().body(matchesJsonSchemaInClasspath("schemas/pdmetalist_schema.json"));
+        String expectedJson = IOUtils.toString(
+                getClass().getClassLoader().getResourceAsStream("response_validation/pdmetalist_res.json"),
+                StandardCharsets.UTF_8
+        );
+        String actualJson = response.getBody().asString();
+        // Remove "timestamp" key from both JSONs
+        JSONObject expectedJsonObject = new JSONObject(expectedJson);
+        expectedJsonObject.remove("timestamp");
+        expectedJson = expectedJsonObject.toString();
+        JSONObject actualJsonObject = new JSONObject(actualJson);
+        actualJsonObject.remove("timestamp");
+        actualJson = actualJsonObject.toString();
+        JSONAssert.assertEquals(expectedJson, actualJson, true);
+    }
+    @Test(priority = 22)
+    public void verify_disb_favoring_approval_api() throws IOException {
+        validateApiResponse(
+                baseUrl + "/ilos/v1/disbursement/favoring-approvals",
+                Map.of("obj_id", objId, "role", "GLOBAL_READ_ONLY"),
+                "schemas/disb_favoring_approval_schema.json",
+                "response_validation/disb_favoring_approval_res.json"
+        );
+    }
+    @Test(priority = 23)
+    public void verify_repayment_bank_account_api() throws IOException {
+        validateApiResponse(
+                baseUrl + "/ilos/v1/repayment/bank-accounts/"+ objId,
+                null,
+                "schemas/repayment_bank_account_schema.json",
+                "response_validation/repayment_bank_account_res.json"
+        );
+    }
+    @Test(priority = 24)
+    public void verify_disb_fav_details_favType_cust_api() throws IOException {
+        validateApiResponse(
+                baseUrl + "/ilos/v1/disbursement/favoring-details/key",
+                Map.of("favoring_type", "CUSTOMER", "obj_id", objId, "role", "GLOBAL_READ_ONLY"),
+                "schemas/disb_fav_details_favType_cust_schema.json",
+                "response_validation/disb_fav_details_favType_cust_res.json"
+        );
+    }
+    @Test(priority = 25)
+    public void verify_disb_fav_details_favType_insurance_api() throws IOException {
+        validateApiResponse(
+                baseUrl + "/ilos/v1/disbursement/favoring-details/key",
+                Map.of("favoring_type", "INSURANCE_VENDOR", "obj_id", objId, "role", "GLOBAL_READ_ONLY"),
+                "schemas/disb_fav_details_favType_ins_schema.json",
+                "response_validation/disb_fav_details_favType_ins_res.json"
+        );
+    }
+    @Test(priority = 26)
+    public void verify_disb_fav_details_favType_SELLER_api() throws IOException {
+        validateApiResponse(
+                baseUrl + "/ilos/v1/disbursement/favoring-details/key",
+                Map.of("favoring_type", "SELLER", "obj_id", objId, "role", "GLOBAL_READ_ONLY"),
+                "schemas/disb_fav_details_favType_seller_schema.json",
+                "response_validation/disb_fav_details_favType_seller_res.json"
+        );
+    }
+    @Test(priority = 27)
+    public void verify_disb_fav_details_api() throws IOException {
+        validateApiResponse(
+                baseUrl + "/ilos/v1/disbursement/favoring-details",
+                Map.of("obj_id", objId, "role", "GLOBAL_READ_ONLY"),
+                "schemas/disb_fav_details_schema.json",
+                "response_validation/disb_fav_details_res.json"
+        );
+    }
+    @Test(priority = 28)
+    public void verify_disb_get_generated_docs_api() throws IOException {
+        validateApiResponse(
+                baseUrl + "/ilos/v1/disbursement/get-generated-docs",
+                Map.of("obj_id", objId),
+                "schemas/disb_get_generated_docs_schema.json",
+                "response_validation/disb_get_generated_docs_res.json"
+        );
+    }
+    @Test(priority = 29)
+    public void verify_disb_docs_esign_history_api() throws IOException {
+        validateApiResponse(
+                baseUrl + "/ilos/v1/disbursement/docs-esign-history",
+                Map.of("obj_id", objId),
+                "schemas/disb_docs_esign_history_schema.json",
+                "response_validation/disb_docs_esign_history_res.json"
+        );
+    }
+    @Test(priority = 30)
+    public void verify_enach_api() throws IOException {
+        validateApiResponse(
+                baseUrl + "/ilos/v1/enach/applications/"+ objId,
+                null,
+                "schemas/enach_schema.json",
+                "response_validation/enach_res.json"
+        );
+    }
+    
 
 }
